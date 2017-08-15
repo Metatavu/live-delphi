e5edb009f8b2697f79d2b56d853f3a5c80b2ce60
Antti Leppä <antti.leppa@metatavu.fi>
2017-08-15 17:51:15 +0300

    Added support for client side templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,33 @@
 /*global module:false*/
 
+const _ = require('lodash');
 const fs = require('fs');
 const util = require('util');
+const pug = require('pug');
+const path = require('path');
 
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
+  
+  grunt.registerMultiTask('compile-client-templates', 'Compiles client pug templates', function () {
+    const clientTemplates = fs.readdirSync(this.data.sourceFolder);
+    const compiledClientTemplates = [];
+    
+    for (let i = 0; i < clientTemplates.length; i++) {
+      let baseName = clientTemplates[i].replace('.pug', '');
+      baseName = `${baseName[0].toUpperCase()}${baseName.substring(1)}`;
+      const templateName = _.camelCase(`${this.data.templatePrefix}${baseName}`);
+      compiledClientTemplates.push(pug.compileFileClient(this.data.sourceFolder + clientTemplates[i], { name: templateName, compileDebug: false }));
+    }
+    
+    const destDir = path.dirname(this.data.destFile);
+    
+    if (!fs.existsSync(destDir)){
+      fs.mkdirSync(destDir);
+    }
+    
+    fs.writeFileSync(this.data.destFile, compiledClientTemplates.join(''));
+  });
   
   grunt.initConfig({
     'sass': {
@@ -19,6 +42,13 @@ module.exports = function(grunt) {
           dest: 'public/css',
           ext: '.min.css'
         }]
+      }
+    },
+    'compile-client-templates': {
+      'compile': {
+        'sourceFolder': __dirname + '/client-src/templates/',
+        'destFile': __dirname + '/public/js/pug-templates.js',
+        'templatePrefix': 'pug'
       }
     },
     'babel': {
@@ -38,5 +68,5 @@ module.exports = function(grunt) {
     }
   });
   
-  grunt.registerTask('default', [ 'sass:client', 'babel:client' ]);
+  grunt.registerTask('default', [ 'sass:client', 'compile-client-templates:compile', 'babel:client' ]);
 };
