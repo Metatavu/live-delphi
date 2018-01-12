147ab5b5f94b32fcf643e69bf5b34f20de25513c
Antti Leppä <antti.leppa@metatavu.fi>
2018-01-12 18:08:03 +0200

    Added plugin for printing PDF files

---
+++ plugins/live-delphi-pdf/index.js
@@ -0,0 +1,62 @@
+/* jshint esversion: 6 */
+/* global __dirname, Promise */
+(() => {
+  'use strict';
+  
+  const pdf = require('html-pdf');
+  
+  /**
+   * PDF rendering functionalities for Live delphi
+   */
+  class Pdf {
+    
+    /**
+     * Constructor
+     * 
+     * @param {Object} logger logger
+     */
+    constructor (logger) {
+      this.logger = logger;
+    }
+    
+    /**
+     * Renders PDF from HTML
+     * 
+     * @param {String} html html string
+     * @param {String} baseUrl base url
+     * @param {String} cookie header (optiona)
+     * @return {Promise} promise of pdf stream
+     */
+    renderPdf(html, baseUrl, cookieHeader) {
+      return new Promise((resolve, reject) => {
+        const options = {
+          "base": baseUrl
+        };
+        
+        if (cookieHeader) {
+          options.httpHeaders = {
+            "Cookie": cookieHeader
+          };
+        }
+        
+        pdf.create(html, options).toStream((err, stream) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(stream);
+          }
+        });
+      });
+    }
+  } 
+  
+  module.exports = (options, imports, register) => {
+    const logger = imports['logger'];
+    const pdf = new Pdf(logger);
+    
+    register(null, {
+      'live-delphi-pdf': pdf
+    });
+  };
+  
+})();
